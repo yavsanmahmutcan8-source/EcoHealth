f4d67089ac7b67b774bbb2da77783ad6eb7c7ec2
yavsanmahmutcan8-source <yavsanmahmutcan8@gmail.com>
2026-04-27 13:18:56 +0300

    chore(db): Add manual migration for PostGIS and fix env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -49,11 +49,17 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_object=include_object,
     )
 
     with context.begin_transaction():
         context.run_migrations()
 
+
+def include_object(object, name, type_, reflected, compare_to):
+    if type_ == "table" and name == 'spatial_ref_sys':
+        return False
+    return True
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
@@ -70,7 +76,9 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection, 
+            target_metadata=target_metadata,
+            include_object=include_object
         )
 
         with context.begin_transaction():
